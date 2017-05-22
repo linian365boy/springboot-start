e8297d6071bd9b918e263c16f15d4cc93b7e11a5
tanfan <tanfan@juanpi.com>
2017-05-22 20:03:42 +0800

    feat(start): mybatis注解改为xml配置

--- src/main/java/com/springboot/dao/CityDao.java
+++ src/main/java/com/springboot/dao/CityDao.java
@@ -17,19 +17,5 @@ import org.apache.ibatis.annotations.*;
  * Created by xchunzhao on 02/05/2017.
  */
 public interface CityDao {
-
-    /**
-     * 根据城市名称，查询城市信息
-     *
-     * @param cityName 城市名
-     */
-    @Select("SELECT * FROM city where city_name=#{cityName}")
-// 返回 Map 结果集
-    @Results({
-            @Result(property = "id", column = "id"),
-            @Result(property = "provinceId", column = "province_id"),
-            @Result(property = "cityName", column = "city_name"),
-            @Result(property = "description", column = "description"),
-    })
     City findByName(@Param("cityName") String cityName);
 }
